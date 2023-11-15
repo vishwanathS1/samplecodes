4b20a4af2fb12e747ebaae044fc442c4fbe1fa09
vishwanathS1 <vishwanath.sightspectrum@gmail.com>
2023-11-15 15:40:57 +0530

    first

---
+++ control strucrure.js/if else switch.js
@@ -0,0 +1,22 @@
+
+/*function checkGrade(grade) {
+    
+    if (grade >= 90) {
+      console.log("A");
+    } 
+    else if (grade >= 80 && grade < 90) {
+      console.log("B");
+    } 
+    else if (grade >= 70 && grade < 80) {
+      console.log("C");
+    } 
+    else if (grade >= 60 && grade < 70) {
+      console.log("D");
+    }
+     else {
+      console.log("F");
+    }
+  }
+  console.log(checkGrade(60));
+*/
+
---
+++ js.js
@@ -0,0 +1,16 @@
+let na="vishwa";
+console.log("hi im"+na+"howare u");
+alert("vishwa");
+//variables(var,let,const)
+var nama="v";
+console.log("hi "+nama+"handsome");
+let nams ="jas";
+document.write("hlo"+ nams +"nice to meet u");
+let color="blue";
+color="red";
+console.log(color);
+const colors="blue";
+console.log(colors)
+//
+let
+
