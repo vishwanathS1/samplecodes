0d6beaa40f4a6ff683631041df62f479cdaef139
vishwanathS1 <149649724+vishwanathS1@users.noreply.github.com>
2023-11-22 18:29:54 +0530

    Add files via upload

--- string.js
+++ string.js
@@ -1,29 +1,37 @@
-// String manipulation program
-function stringManipulation() {
-    // Concatenation
-    const firstName = 'John';
-    const lastName = 'Doe';
-    const fullName = firstName + ' ' + lastName;
-    console.log('Full Name:', fullName);
+console.log("string methods & search");
+let str1="welcome to vishwa"
+let str2="my name is jhon cena"
+let numb=77
+let h = new String("John");
+console.log(h);
+console.log(h.length);
+console.log(str1.charAt(6));
+console.log(str1.charCodeAt(5));
+console.log(str1.codePointAt(6));
+console.log(str1.concat(str2));
+console.log(str1.endsWith("world"));
+console.log(str1.endsWith("my"))
+console.log(str1.startsWith("my"))
+console.log(str1.startsWith("to"))
+console.log(str1.includes("my"));//method) String.includes(searchString: string, position?: number | undefined): boolean
+console.log(str1.lastIndexOf("to"));
+console.log(str1.indexOf("my"))//(method) String.indexOf(searchString: string, position?: number | undefined): number
+console.log(str1.match("me"))//match(matcher: { [Symbol.match](string: string): RegExpMatchArray | null; }): RegExpMatchArray | null
+///m/,/m/g,/m/gi
+console.log(str1.matchAll("me"))
+console.log(str1.localeCompare());//method) String.localeCompare(that: string): number (+1 overload
+console.log(str1.normalize())//normalize(form?: string | undefined): string
+console.log(str1.padEnd(29,"*"));//padEnd(maxLength: number, fillString?: string | undefined): string
+console.log(str1.padStart(29,"#"));//padStart(maxLength: number, fillString?: string | undefined): string
+console.log(str1.repeat(5));//repeat(count: number): string
+console.log(str1.replace("world","home"));//replace(searchValue: { [Symbol.replace](string: string, replaceValue: string): string; }, replaceValue: string): string
+console.log(str1.search("my"));//method) String.search(regexp: string | RegExp): number (+1 overload)
+console.log(str1.slice(4,12));//lice(start?: number | undefined, end?: number | undefined): string
+console.log(str1.split("+"));//split(splitter: { [Symbol.split](string: string, limit?: number | undefined): string[]; }, limit?: number | undefined): string[]
+console.log(str1.substring(3,19));//substring(start: number, end?: number | undefined): string
+console.log(str1.toLocaleLowerCase());//toLocaleLowerCase(locales?: string | string[] | undefined): string
+console.log(str1.toLocaleUpperCase());//toLocaleUpperCase(locales?: string | string[] | undefined): string
+console.log(str1.toLowerCase());
+console.log(str1.toUpperCase());
+console.log(str1.valueOf());
 
-    // String length
-    const message = 'Hello, World!';
-    console.log('Length of the message:', message.length);
-
-    // Accessing characters
-    const firstCharacter = message[0];
-    const lastCharacter = message[message.length - 1];
-    console.log('First character:', firstCharacter);
-    console.log('Last character:', lastCharacter);// Substring
-    const substringExample = message.substring(0, 5);
-    console.log('Substring:', substringExample);
-const uppercaseMessage = message.toUpperCase();
-    const lowercaseMessage = message.toLowerCase();
-    console.log('Uppercase message:', uppercaseMessage);
-    console.log('Lowercase message:', lowercaseMessage);
-
-    // Replace
-    const replacedMessage = message.replace('World', 'Universe');
-    console.log('Replaced message:', replacedMessage);
-}
-stringManipulation();
---
+++ templates.js
@@ -0,0 +1,13 @@
+
+let text=`i'm a software developer`
+let text1=`hii
+welcome 
+my name is    rock`
+let text2=`hiii
+    
+             all is well
+                     all so bueatifull`
+console.log(text);
+console.log(text1);
+console.log(text2);
+
