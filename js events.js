266851571ec878338c9de2e490c4a39c17e7e1e2
vishwanathS1 <149649724+vishwanathS1@users.noreply.github.com>
2023-11-24 18:04:49 +0530

    Add files via upload

---
+++ js events.js
@@ -0,0 +1,11 @@
+function handleClick() {
+    alert('Button clicked!');
+  }
+  function handleMouseOver() {
+    console.log('Mouse over the element');
+  }
+  
+  function handleMouseOut() {
+    console.log('Mouse out of the element');
+  }
+    
