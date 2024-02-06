db891fc9743ec0ff5f9a4a2651c91e856cc28fcb
vishwanathS1 <149649724+vishwanathS1@users.noreply.github.com>
2024-02-06 10:17:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import SignIn from './compone/SignIn';
+import SignUp from './compone/SignUp';
+
+
+function App() {
+  return (
+    <div className="App">
+    <SignUp/>
+    {/* <SignIn/> */}
+
+   
+     </div>
+  );
+}
+
+export default App;
+   
---
+++ src/compone/SignIn.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import bgImg from '../assests/ats1.jpg'; 
+import {useForm} from 'react-hook-form';
+import axios from 'axios';
+import {Avatar} from '@mui/material'
+
+export default function SignIn() {
+
+  const{register,handleSubmit,watch,formState:{errors}}=useForm()
+//   const onSubmit= data => console.log(data);
+ 
+  console.log(watch('username'));
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios.post('https://65bc783652189914b5bdee93.mockapi.io/ATS ', data);
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error submitting the form:', error);
+    }
+    console.log('values:',data);
+  };
+
+  console.log(watch('username'));
+
+  return (
+   <section>
+    <div className="register">
+    <div className='col-2'>
+        <img src={bgImg} alt=''/>
+    </div>
+    <div className='col-1'>
+    <Avatar src='./img1.jpeg' style={{marginLeft:'125px',padding:'10px'}} />
+        <h2></h2>
+        <span>Login</span>
+        <form id='form' className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
+            <input type='text' {...register("username")} placeholder='username'/>
+            <input type='text' {...register("password")}placeholder='password'/>
+            {errors.mobile?.type === "required" && "mobile number is required"}
+            {errors.mobile?.type === "maxLength" && "max length exceed"}
+
+            <button className='btn' >Sign In</button>
+        </form>
+    </div>
+   
+    </div>
+   </section>
+  )
+}
